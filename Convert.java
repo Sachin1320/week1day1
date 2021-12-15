fed4c70aefc0ec2c66c13e47d54e17b5fcf730de
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-12-15 16:52:50 +0530

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Convert {
+		public static void main(String[] args) {
+			int num1=-40;
+	        System.out.println();
+	        int num2 =Math.abs(num1);
+	        System.out.println(num2);
+
+		}
+	}
+
